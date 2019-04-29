808067657a007ee342422ce0aa6d7b823e47d093
Gabriel Sosa <gabriel.sosa@link-me.it>
2019-04-29 17:38:49 +0200

    move folder

---
+++ prob_1/solution.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"strings"
+	"time"
+)
+
+var pathToQuestions string
+var timePerAnswer int
+
+func main() {
+	parseFlags()
+	file, err := getQuestions()
+	if err != nil {
+		fmt.Println("error")
+	} else {
+		score := answerQestions(file)
+		fmt.Println("your  score is", score)
+	}
+}
+
+func parseFlags() {
+	flag.StringVar(&pathToQuestions, "path", "./problems.csv", "path to the csv file")
+	flag.IntVar(&timePerAnswer, "time", 30, "time available for each answer")
+	flag.Parse()
+}
+
+func getQuestions() ([]byte, error) {
+	return ioutil.ReadFile(pathToQuestions)
+}
+
+func answerQestions(file []byte) (res int) {
+	res = 0
+	r := csv.NewReader(strings.NewReader(string(file)))
+	for {
+		record, err := r.Read()
+		answer := make(chan bool)
+		if err == io.EOF {
+			return
+		}
+		var input string
+		go func() {
+			fmt.Println(record[0])
+			fmt.Scanln(&input)
+			answer <- true
+		}()
+		select {
+		case <-answer:
+			if record[1] == input {
+				res++
+			}
+		case <-time.After(time.Duration(timePerAnswer) * time.Second):
+			return
+		}
+	}
+}
